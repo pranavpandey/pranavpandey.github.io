b53f532684a44abea9722e386cebe408c8bc8336
Pranav Pandey <pranavpande92@gmail.com>
2023-02-06 07:09:25 +0530

    Rework theme and navigation
    Add support for auto theme and save user preference.
    Add support to load navigation paths dynamically from JSON.

--- src/pp-app.js
+++ src/pp-app.js
@@ -12,7 +12,6 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
 import './elements/pp-elements.js';
-import './elements/pp-analytics.js';
 
 // Gesture events like tap and track generated from touch will not be
 // preventable, allowing for better scrolling performance.
@@ -37,25 +36,30 @@ class PPApp extends PolymerElement {
         data="{{routeData}}" tail="{{subroute}}">
       </app-route>
 
+      <app-localstorage-document key="autoTheme" data="{{autoTheme}}">
+      </app-localstorage-document>
+      <app-localstorage-document key="customTheme" data="{{customTheme}}">
+      </app-localstorage-document>
       <app-localstorage-document key="theme" data="{{theme}}">
       </app-localstorage-document>
 
-      <iron-ajax auto url="../data/base64.json" 
-        handle-as="json" last-response="{{dataBase64}}">
+      <iron-ajax auto url="../data/navigation.json" 
+        handle-as="json" last-response="{{navigation}}">
       </iron-ajax>
 
-      <app-drawer-layout responsive-width="700px" fullbleed narrow="{{narrow}}">
+      <app-drawer-layout responsive-width="840px" fullbleed narrow="{{narrow}}">
         <!-- Drawer content -->
         <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
           <div class="scrollable flex">
             <app-toolbar class="drawer-header layout horizontal center-vertical">
               <div>
                 <iron-image class="drawer-header-image" sizing="cover" preload 
-                  placeholder="[[dataBase64.drawerHeader]]" src="../images/drawer.jpg">
+                  placeholder="[[navigation.header.placeholder]]" src="[[navigation.header.icon]]"
+                  hidden$="[[!navigation.header.icon]]">
                 </iron-image>
               </div>
               <div>
-                <p class="drawer-quote">Nothing is perfect but can be better. Developing such better things!</p>
+                <p class="drawer-quote" hidden$="[[!navigation.header.title]]">{{navigation.header.title}}</p>
               </div>
             </app-toolbar>
 
@@ -64,80 +68,71 @@ class PPApp extends PolymerElement {
             <div id="theme-selector">
               <app-toolbar class="drawer-section layout horizontal center-vertical">
                 <div>
-                  <iron-icon class="style color-primary" icon="pp-icons:theme">
+                  <paper-icon-button id="theme-icon"
+                    tabIndex="-1" class="theme color-secondary" 
+                    aria-hidden="true" icon="pp-icons:theme-auto">
+                  </paper-icon-button>
+                  <paper-tooltip aria-label="Auto">Auto</paper-tooltip>
                 </div>
                 <div>
-                  <iron-icon class="theme color-secondary" icon="pp-icons:theme-day">
+                  <iron-icon id="theme-icon-day"
+                    class="theme color-tint-background" 
+                    icon="pp-icons:theme-day">
+                  </iron-icon>
                 </div>
                 <div>
-                  <paper-toggle-button class="theme color-primary"></paper-toggle-button>
+                  <paper-toggle-button id="theme-toggle" 
+                    tabIndex="-1" class="theme color-secondary" 
+                    aria-hidden="true">
+                  </paper-toggle-button>
                 </div>
                 <div>
-                  <iron-icon class="theme color-secondary" icon="pp-icons:theme-night">
+                  <iron-icon id="theme-icon-night"
+                    class="theme color-tint-background" 
+                    icon="pp-icons:theme-night">
+                  </iron-icon>
                 </div>
               </app-toolbar>
 
               <pp-divider></pp-divider>
             </div>
 
-            <div class="drawer-list-container">
-              <iron-selector selected="[[page]]" attr-for-selected="name" 
-                class="drawer-list" role="navigation" on-iron-activate="drawerSelected">
-                <a name="home" href="[[rootPath]]home">
-                <iron-icon icon="pp-icons:home">
-                </iron-icon><paper-ripple class="primary">
-                </paper-ripple>Home
-                </a>
-                <a name="about" href="[[rootPath]]about">
-                  <iron-icon icon="pp-icons:account-circle">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>About
-                </a>
-                <a name="skills" href="[[rootPath]]skills">
-                  <iron-icon icon="pp-icons:stars">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>Skills
-                </a>
-                <a name="projects" href="[[rootPath]]projects">
-                  <iron-icon icon="pp-icons:build">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>Projects
-                </a>
-                <a name="work" href="[[rootPath]]work">
-                  <iron-icon icon="pp-icons:work">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>Work
-                </a>
-                <a name="contact" href="[[rootPath]]contact">
-                  <iron-icon icon="pp-icons:feedback">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>Contact
-                </a>
-              </iron-selector>
-            </div>
+            <template id="pages" is="dom-repeat" items="[[navigation.pages]]" as="pageCategory">
+              <div class="drawer-list-container">
+                <iron-selector selected="[[page]]" attr-for-selected="name" 
+                  class="drawer-list" role="navigation" on-iron-activate="drawerSelected"
+                  selectable="[[pageCategory.selectable]]">
+                  <template id="page" is="dom-repeat" items="[[pageCategory.paths]]" as="pagePath">
+                    <a name="[[pagePath.name]]" href="[[rootPath]][[pagePath.path]]">
+                      <paper-ripple class="selector"></paper-ripple>
+                      <iron-icon icon="[[pagePath.icon]]"></iron-icon>{{pagePath.title}}
+                    </a>
+                  </template>
+                </iron-selector>
+              </div>
 
-            <pp-divider></pp-divider>
+              <pp-divider hidden$="[[!pageCategory.divider]]"></pp-divider>
+            </template>
 
-            <div class="drawer-list-container">
-              <iron-selector class="drawer-list" selectable="false" on-iron-activate="drawerSelected">
-                <!-- <a href="https://techtics.xyz" target="_blank" rel="noopener" tabindex="-1">
-                  <iron-icon icon="social-icons:techtics">
-                  </iron-icon><paper-ripple class="primary">
-                  </paper-ripple>Techtics
-                </a> -->
-                <a href="https://privacy.pranavpandey.com" target="_blank" rel="noopener" tabindex="-1">
-                  <iron-icon class="color-link-privacy" icon="pp-icons:security">
-                  </iron-icon><paper-ripple class="color-link-privacy">
-                  </paper-ripple>Privacy &amp; Terms
-                </a>
-              </iron-selector>
-            </div>
+            <template id="links" is="dom-repeat" items="[[navigation.links]]" as="linkCategory">
+              <div class="drawer-list-container">
+                <iron-selector selected="[[page]]" attr-for-selected="name" 
+                  class="drawer-list" selectable="[[linkCategory.selectable]]">
+                  <template id="link" is="dom-repeat" items="[[linkCategory.paths]]" as="linkPath">
+                    <a name="[[linkPath.name]]" href="[[linkPath.path]]" target="_blank" rel="noopener" tabindex="-1">
+                      <paper-ripple class$="[[linkPath.class]]"></paper-ripple>
+                      <iron-icon class$="[[linkPath.class]]" icon="[[linkPath.icon]]"></iron-icon>{{linkPath.title}}
+                    </a>
+                  </template>
+                </iron-selector>
+              </div>
 
-            <pp-divider></pp-divider>
+              <pp-divider hidden$="[[!linkCategory.divider]]"></pp-divider>
+            </template>
 
             <div class="flex footer">
               <p class="footer" id="copyright"></p>
-              <p class="footer">Android is a trademark of Google LLC.<br>Xperia is a trademark or registered trademark of Sony Mobile Communications Inc.<br>All other logos and trademarks are the property of their respective owners.<br>Views expressed here are mine and not from my employers.</p>
+              <p class="footer" hidden$="[[!navigation.footer]]">{{navigation.footer}}</p>
             </div>
           </div>
         </app-drawer>
@@ -146,29 +141,29 @@ class PPApp extends PolymerElement {
         <app-header-layout>
           <app-header slot="header" condenses fixed effects="material">
             <app-toolbar>
-              <paper-icon-button icon="pp-icons:menu" aria-hidden="true" drawer-toggle></paper-icon-button>
-              <div condensed-title>Pranav Pandey</div>
+              <paper-icon-button icon="pp-icons:menu" aria-label="Toggle navigation drawer" 
+                drawer-toggle></paper-icon-button>
+              <div condensed-title>{{navigation.title}}</div>
               <a class=".drawer-list a" href="https://github.com/pranavpandey" 
-                target="_blank" rel="noopener" tabindex="-1" aria-label="GitHub">
-                <paper-icon-button class="color-tint-primary" icon="social-icons:github" aria-hidden="true">
-              </paper-icon-button></a>
-              <a class=".drawer-list a" href="https://twitter.com/pranavpandeydev" 
-                target="_blank" rel="noopener" tabindex="-1" aria-label="Twitter">
-                <paper-icon-button class="color-tint-primary" icon="pp-icons:person" aria-hidden="true">
-              </paper-icon-button></a>
+                target="_blank" rel="noopener" tabindex="-1" aria-label="Open Source">
+                <paper-icon-button class="color-tint-primary" 
+                  icon="social-icons:github" tabindex="-1" aria-hidden="true">
+                </paper-icon-button>
+                <paper-tooltip aria-label="Open Source">OSS</paper-tooltip>
+              </a>
             </app-toolbar>
             <app-toolbar></app-toolbar>
-            <app-toolbar hidden\$="[[smallLayout]]"></app-toolbar>
+            <app-toolbar hidden$="[[smallLayout]]"></app-toolbar>
             <app-toolbar>
-              <div main-title>Pranav Pandey<br>
-                <span class="main-subtitle">Software Developer &amp; Designer</span>
+              <div main-title>{{navigation.title}}<br>
+                <span class="main-subtitle">{{navigation.subtitle}}</span>
               </div>
             </app-toolbar>
             <app-toolbar></app-toolbar>
           </app-header>
 
           <div>
-            <div class="content-frame" hidden\$="[[loading]]">
+            <div class="content-frame" hidden$="[[loading]]">
               <iron-pages class="content" role="main" selected="[[page]]" 
                 attr-for-selected="name" fallback-selection="404">
                 <pp-home theme="[[theme]]" loading="{{loading}}" name="home"></pp-home>
@@ -181,21 +176,22 @@ class PPApp extends PolymerElement {
               </iron-pages>
             </div>
 
-            <div class="progress" hidden\$="[[!loading]]">
-              <paper-spinner active\$="[[loading]]"></paper-spinner>
+            <div class="progress" hidden$="[[!loading]]">
+              <paper-spinner active$="[[loading]]"></paper-spinner>
             </div>
           </div>
         </app-header-layout>
 
         <a class=".drawer-list a" href="https://play.google.com/store/apps/dev?id=6608630615059087491" 
           target="_blank" rel="noopener" tabindex="-1" aria-label="Google Play">
-          <paper-fab icon="pp-icons:shop" title="Google Play">
-        </paper-fab></a>
+          <paper-fab icon="pp-icons:shop" title="Google Play"></paper-fab>
+        </a>
       </app-drawer-layout>
 
       <paper-toast id="toast"></paper-toast>
 
-      <iron-media-query query="(max-width:700px)" query-matches="{{smallLayout}}">
+      <iron-media-query query="(max-width:840px)" 
+        query-matches="{{smallLayout}}">
       </iron-media-query>
     `;
   }
@@ -209,11 +205,23 @@ class PPApp extends PolymerElement {
       },
       routeData: Object,
       subroute: String,
+      autoTheme: {
+        type: Boolean,
+        value: true,
+        reflectToAttribute: true,
+        observer: '_refreshTheme'
+      },
+      customTheme: {
+        type: String,
+        value: 'light',
+        reflectToAttribute: true,
+        observer: '_refreshTheme'
+      },
       theme: {
         type: String,
-        value: "light",
+        value: 'light',
         reflectToAttribute: true,
-        observer: '_themeChanged'
+        observer: '_refreshTheme'
       },
       smallLayout: {
         type: Boolean,
@@ -243,32 +251,84 @@ class PPApp extends PolymerElement {
       this._showToast(e);
     });
 
-    this.shadowRoot.querySelector('paper-toggle-button')
+    window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener("change", (e) => {
+        this.setTheme();
+    });
+
+    this.shadowRoot.querySelector('#theme-icon')
+      .addEventListener('click', (e) => {
+        this.set('autoTheme', !this.autoTheme);
+    });
+
+    this.shadowRoot.querySelector('#theme-toggle')
       .addEventListener('change', (e) => {
-        this.setTheme(null, e.target.checked);
-    }); 
+        this.set('customTheme', e.target.checked ? 'dark' : 'light');
+    });
   }
 
-  _themeChanged(theme, reload) {
+  applyTheme(theme) {
     if (theme === 'dark') {
       this.updateStyles(this.getDarkTheme());
-      this.shadowRoot.querySelector('paper-toggle-button').checked = true;
     } else {          
       this.updateStyles(this.getLightTheme());
-      this.shadowRoot.querySelector('paper-toggle-button').checked = false;
-    }
-  }
-  
-  setTheme(theme, dark) {
-    if (theme === 'dark' || dark) {
-      this.set('theme', 'dark');
-    } else {          
-      this.set('theme', 'light');
     }
 
-    if (this.reloadOnThemeChange()) {
-      window.location.reload();
+    this.set('theme', theme);
+  }
+  
+  _refreshTheme() {
+    this.shadowRoot.querySelector('#theme-toggle').checked = this.customTheme === 'dark';
+
+    if (this.autoTheme) {
+      this.shadowRoot.querySelector('#theme-icon-day').style.opacity = 0.5;
+      this.shadowRoot.querySelector('#theme-toggle').disabled = true;
+      this.shadowRoot.querySelector('#theme-icon-night').style.opacity = 0.5;
+    
+      this.shadowRoot.querySelector('#theme-icon')
+        .updateStyles(
+          {
+            'color': 'var(--dt-secondary-color)',
+            '--paper-icon-button-ink-color': 'var(--dt-secondary-color)'
+          }
+        );
+    } else {
+      this.shadowRoot.querySelector('#theme-icon-day').style.opacity = 1;
+      this.shadowRoot.querySelector('#theme-toggle').disabled = false;
+      this.shadowRoot.querySelector('#theme-icon-night').style.opacity = 1;
+
+      this.shadowRoot.querySelector('#theme-icon')
+        .updateStyles(
+          {
+            'color': 'var(--dt-tint-background-color)',
+            '--paper-icon-button-ink-color': 'var(--dt-tint-background-color)'
+          }
+        );
     }
+
+    this.setTheme();
+  }
+
+  setTheme() {
+    let currentTheme = 'light';
+
+    if (this.autoTheme) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        currentTheme = 'dark';
+      } else if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
+        let hours = new Date().getHours();
+
+        if (hours < 6 || hours >= 19) {
+          currentTheme = 'dark';
+        }
+      }
+    } else {
+      if (this.customTheme === 'dark') {
+        currentTheme = 'dark';
+      }
+    }
+
+    this.applyTheme(currentTheme);
   }
 
   _routePageChanged(page) {
@@ -292,33 +352,27 @@ class PPApp extends PolymerElement {
         pageTitle = '';
         break;
       case 'about':
-        pageTitle = 'About - ';
+        pageTitle = 'About | ';
         break;
       case 'skills':
-        pageTitle = 'Skills - ';
+        pageTitle = 'Skills | ';
         break;
       case 'projects':
-        pageTitle = 'Projects - ';
+        pageTitle = 'Projects | ';
         break;
       case 'work':
-        pageTitle = 'Work - ';
+        pageTitle = 'Work | ';
         break;
       case 'contact':
-        pageTitle = 'Contact - ';
+        pageTitle = 'Contact | ';
         break;
       case '404':
-        pageTitle = 'Error - ';
+        pageTitle = 'Error | ';
         break;
     }
 
     document.title = pageTitle + 'Pranav Pandey';
     import(`./pages/pp-${page}.js`).then(null, this._showPage404.bind(this));
-
-    // Track page via analytics.
-    this.dispatchEvent(new CustomEvent('on-track-page', {
-      bubbles: true, composed: true, 
-      detail: { path: window.location.pathname }
-    }));
   }
 
   _showPage404() {
@@ -342,36 +396,44 @@ class PPApp extends PolymerElement {
   getLightTheme() {
     return { '--dt-theme': 'light',
       '--dt-background-color': '#EAEAEA',
-      '--dt-surface-color': '#EEEEEE',
-      '--dt-selector-color': '#DADADA',
+      '--dt-surface-color': '#EBEBEB',
+      '--dt-surface-color-elevation': '#EBEBEB',
       '--dt-header-color': '#3F51B5',
       '--dt-primary-color': '#3F51B5',
       '--dt-secondary-color': '#E91E63',
-      '--dt-tint-primary-color': '#FFFFFF',
-      '--dt-tint-secondary-color': '#FFFFFF',
-      '--dt-text-primary-color': '#000000',
-      '--dt-text-secondary-color': '#454545',
-      '--dt-text-description-color': '#787878',
+      '--dt-tint-background-color': '#414141',
+      '--dt-tint-primary-color': '#C9CEEA',
+      '--dt-tint-secondary-color': '#F8C0D3',
+      '--dt-text-primary-color': '#121212',
+      '--dt-text-secondary-color': '#343434',
+      '--dt-text-description-color': '#676767',
+      '--dt-link-techtics-color': '#138808',
+      '--dt-link-support-color': '#4285F4',
+      '--dt-link-theme-color': '#9C27B0',
       '--dt-link-privacy-color': '#1976D2',
-      '--pp-divider-color': '#CACACA'
+      '--pp-divider-color': '#BCBCBC'
     };
   }
 
   getDarkTheme() {
     return { '--dt-theme': 'dark', 
-      '--dt-background-color': '#222222',
-      '--dt-surface-color': '#343434',
-      '--dt-selector-color': '#323232',
+      '--dt-background-color': '#252525',
+      '--dt-surface-color': '#2D2D2D',
+      '--dt-surface-color-elevation': '#353535',
       '--dt-header-color': '#3F51B5',
-      '--dt-primary-color': '#7986CB',
-      '--dt-secondary-color': '#FF4081',
-      '--dt-tint-primary-color': '#FFFFFF',
-      '--dt-tint-secondary-color': '#FFFFFF',
-      '--dt-text-primary-color': '#FFFFFF',
+      '--dt-primary-color': '#3F51B5',    
+      '--dt-secondary-color': '#FBC02D',
+      '--dt-tint-background-color': '#C1C1C1',
+      '--dt-tint-primary-color': '#C9CEEA',
+      '--dt-tint-secondary-color': '#46350C',
+      '--dt-text-primary-color': '#EFEFEF',
       '--dt-text-secondary-color': '#CDCDCD',
-      '--dt-text-description-color': '#787878',
+      '--dt-text-description-color': '#898989',
+      '--dt-link-techtics-color': '#FF9933',
+      '--dt-link-support-color': '#3DDC84',
+      '--dt-link-theme-color': '#CDDC39',
       '--dt-link-privacy-color': '#2196F3',
-      '--pp-divider-color': '#4A4A4A'
+      '--pp-divider-color': '#454545',
     };
   }
 
